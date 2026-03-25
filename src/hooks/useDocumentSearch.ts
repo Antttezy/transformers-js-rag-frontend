c731fb4f0b3d3a709a289b10052db37ae4bab439
Antttezy <antkumachev@gmail.com>
2026-03-25 21:54:33 +0300

    Add document search request

--- src/hooks/useDocumentSearch.ts
+++ src/hooks/useDocumentSearch.ts
@@ -1,9 +1,12 @@
 import { useMemo } from "react";
 import { DocumentSearchAPI } from "../llm/DocumentSearch";
 
-
 export function useDocumentSearch() {
+    const url = import.meta.env.VITE_APP_DOCUMENT_SEARCH_BACKEND_URL as (string | undefined)
+    
     return useMemo(() => {
-        return new DocumentSearchAPI()
-    }, [])
+        if (!url) return null
+
+        return new DocumentSearchAPI(url)
+    }, [url])
 }
--- src/llm/DocumentSearch.ts
+++ src/llm/DocumentSearch.ts
@@ -1,7 +1,37 @@
 import type { DocumentSearch } from "./types";
 
 export class DocumentSearchAPI implements DocumentSearch {
-    async search(): Promise<string[]> {
-        return ["Демо документ 1", "Демо документ 2"]
+    private endpoint: string
+
+    constructor(endpoint: string) {
+        console.log("DocumentSearch", "endpoint", endpoint)
+        this.endpoint = endpoint
     }
+
+    async search(query: string): Promise<string[]> {
+        const docs = await this.retrieveContext(query)
+        return docs.results.map(d => d.text)
+    }
+
+    private async retrieveContext(query: string): Promise<RetrieveContextResponse> {
+        const url = `${this.endpoint}/retrieve?query=${query}`
+
+        const response = await fetch(url, { redirect: 'follow' }).then(r => {
+            if (r.status != 200)
+                throw new Error(`request failed with code: ${r.status}`)
+
+            return r.json()
+        })
+
+        return response as RetrieveContextResponse
+    }
+}
+
+type RetrieveContextResponse = {
+    results: {
+        doc_id: string
+        text: string
+        distance: number
+        metadata?: Record<string, any> /* eslint-disable-line @typescript-eslint/no-explicit-any */
+    }[]
 }
